491bf59a79456fe5bf6125e84bb54c7128da40f6
sungyeong98 <am25227903@gmail.com>
2024-12-13 12:16:13 +0900

    feat: 레포지터리 구현

---
+++ src/main/java/com/mysite/sbb_practice2/answer/AnswerRepository.java
@@ -0,0 +1,6 @@
+package com.mysite.sbb_practice2.answer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+}
