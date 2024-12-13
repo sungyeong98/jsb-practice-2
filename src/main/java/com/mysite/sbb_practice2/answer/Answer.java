4552238f1a3bd95adbef05c4b5b3ce3faccf613b
sungyeong98 <am25227903@gmail.com>
2024-12-13 12:16:00 +0900

    feat: 엔티티 설정

---
+++ src/main/java/com/mysite/sbb_practice2/answer/Answer.java
@@ -0,0 +1,28 @@
+package com.mysite.sbb_practice2.answer;
+
+import com.mysite.sbb_practice2.question.Question;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.persistence.ManyToOne;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+
+}
